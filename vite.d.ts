fa713ca4cab25f77e5e61047085e82d2d12d015d
张三 <2306860505@qq.com>
2024-11-01 18:17:34 +0800

    移除不应该出现的提交

--- vite.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: (reg?: RegExp) => {
-    name: string;
-    transform(_: any, id: string): {
-        code: string;
-        map: null;
-    } | undefined;
-};
-export default _default;
